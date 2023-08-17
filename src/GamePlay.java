7d10e41ec9c3062cfa576fbb823cc7ee79f86774
Sohan <Sohanoffice46@gmail.com>
2023-08-17 21:04:22 +0530

    Created Game objects

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -22,25 +22,31 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int ballXdir = -1;
     private int ballYdir = -2;
 
+    private MapGenerator map;
+
     public GamePlay() {
+        map = new MapGenerator(3, 7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer = new timer(delay, this);
+        timer = new Timer(delay, this);
         timer.start();
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
 
         // background
         g.setColor(Color.BLACK);
-        g.fillRect(1,1,692, 592);
+        g.fillRect(1, 1, 692, 592);
+
+        // map
+        map.draw((Graphics2D) g);
 
         //borders
         g.setColor(Color.YELLOW);
-        g.fillRect(0,0,3,592);
-        g.fillRect(0,0,692,3);
-        g.fillRect(691,0,3,592);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
 
         //paddle
         g.setColor(Color.GREEN);
@@ -48,13 +54,39 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
         //ball
         g.setColor(Color.RED);
-        g.fillRect(ballposX, ballposY, 20, 20);
-    }
+        g.fillOval(ballposX, ballposY, 20, 20);
 
+        g.dispose();
+    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.start();
+        if (play) {
+            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550,100, 8))) {
+                ballYdir = -ballYdir;
+            }
+
+
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+
+            if(ballposX < 0) {
+                ballXdir = -ballXdir;
+            }
+
+            if (ballposY < 0) {
+                ballYdir = -ballYdir;
+            }
+
+            if(ballposX > 670) {
+                ballXdir = -ballXdir;
+            }
+
+
+        }
+        repaint();
 
     }
 
@@ -64,12 +96,35 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyReleased(KeyEvent e) {
 
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
 
+    public void moveRight() {
+        play = true;
+        playerX+=20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX-=20;
     }
 }
--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -1,13 +1,32 @@
+import java.awt.*;
 import java.util.Map;
 
 public class MapGenerator {
+
     public int map[][];
     public int brickWidth;
     public int brickHeight;
+
     public MapGenerator(int row, int col) {
-        map = new int[][];
-        for (int  []map1 : map) {
-            for (int j=0; j<map[0].length; j++)
+        map = new int[row][col];
+       for (int i = 0; i < map.length; i++) {
+           for(int j = 0; j<map[0].length; j++) {
+               map[i][j] = 1;
+           }
+       }
+
+       brickWidth = 540/col;
+       brickHeight = 240/row;
+    }
+
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
         }
     }
 }
